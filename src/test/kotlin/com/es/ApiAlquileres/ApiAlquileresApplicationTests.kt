799d1f3854024984ac4765d14887194c85d2c1e1
dganicolas <dganicolas@rafaelalberti.es>
2024-12-12 03:25:31 +0100

    commit 1: comienza la aventura

---
+++ src/test/kotlin/com/es/ApiAlquileres/ApiAlquileresApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.es.ApiAlquileres
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiAlquileresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
