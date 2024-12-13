88ff1ff2245c0acff104f26b486dfc0319a072a5
dganicolas <dganicolas@rafaelalberti.es>
2024-12-13 02:59:06 +0100

    commit 3: la he liado, nose como merge a la rama master, seguimos trabajando aqui :D

--- src/main/kotlin/com/es/apiGestorDeEventos/controller/UsuarioController.kt
+++ src/main/kotlin/com/es/apiGestorDeEventos/controller/UsuarioController.kt
@@ -57,7 +57,7 @@ class UsuarioController {
             return ResponseEntity(mapOf("mensajes" to "Credenciales incorrecta dude"), HttpStatus.UNAUTHORIZED)
         }
 
-        // si pasamos la autenticacion, significa que estamos bien autenticados
+        // si pasamos la autenticacion, significa que estamos bien autenticados Dx
         //paamos a generar el token
         var token = ""
         token = tokenService.generarToken(authentication)
